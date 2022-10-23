a03616e385de8f2755a829080848861909e72457
Stan Cardinaels <stancardinaels@hotmail.com>
2022-10-23 14:44:40 +0200

    Deleted lap-chart, increased numbers

--- competition/vue-app/src/views/StatisticsView.vue
+++ competition/vue-app/src/views/StatisticsView.vue
@@ -1,115 +1,102 @@
 <template>
-  <v-container>
-    <v-row justify="center">
-      <v-col cols="12" sm="10" md="6" lg="3">
-        <v-simple-table class="tertiary">
-          <thead>
-            <tr>
-              <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Queue</th>
-            </tr>
-          </thead>
-          <tbody>
-            <queue :range-from="0" :range-to="3" dense :lg="11" :md="11" :sm="11"/>
-          </tbody>
-        </v-simple-table>
-      </v-col>
-      <v-col cols="12" sm="10" md="6" lg="3">
-        <v-simple-table class="tertiary">
-          <thead>
-            <tr>
-              <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Top runners</th>
-            </tr>
-          </thead>
-          <tbody>
-            <top-runners :max="3" dense/>
-          </tbody>
-        </v-simple-table>
-      </v-col>
-      <v-col cols="12" sm="10" md="6" lg="3">
-        <v-simple-table class="tertiary">
-          <thead>
-            <tr>
-              <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Top groups</th>
-            </tr>
-          </thead>
-          <tbody>
-            <top-groups :max="3" dense/>
-          </tbody>
-        </v-simple-table>
-      </v-col>
-      <v-col cols="12" sm="10" md="6" lg="3">
-        <v-simple-table class="tertiary">
-          <thead>
-            <tr>
-              <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Most active runners</th>
-            </tr>
-          </thead>
-          <tbody>
-            <most-active-runners :max="3" dense/>
-          </tbody>
-        </v-simple-table>
-      </v-col>
-      <v-col cols="12" sm="10" md="6" lg="3">
-        <v-row>
-          <v-col cols="12">
-            <v-simple-table class="tertiary">
-              <thead>
-                <tr>
-                  <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Current runner</th>
-                </tr>
-              </thead>
-              <tbody>
-                <active-runner>
-                  <template v-slot:full="{ loading, lap }">
-                    <v-container v-show="loading">
-                      <v-row justify="center">
-                        <v-progress-circular indeterminate color="primary" />
-                      </v-row>
-                    </v-container>
-                    <v-list v-show="!loading" color="tertiary">
-                      <v-list-item>
-                        <v-container>
-                          <v-card class="d-flex" justify="center" color="tertiary">
+    <v-container>
+        <v-row justify="center">
+            <v-col cols="12" sm="10" md="6" lg="3">
+                <v-simple-table class="tertiary">
+                <thead>
+                    <tr>
+                    <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Queue</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <queue :range-from="0" :range-to="10" dense :lg="11" :md="11" :sm="11"/>
+                </tbody>
+                </v-simple-table>
+            </v-col>
+            <v-col cols="12" sm="10" md="6" lg="3">
+                <v-simple-table class="tertiary">
+                    <thead>
+                        <tr>
+                        <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Top runners</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <top-runners :max="5" dense/>
+                    </tbody>
+                </v-simple-table>
+                <v-simple-table class="tertiary">
+                    <thead>
+                        <tr>
+                        <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Current runner</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <active-runner>
+                        <template v-slot:full="{ loading, lap }">
+                            <v-container v-show="loading">
                             <v-row justify="center">
-                              <v-col>
-                                <v-card-text class="text-center py-0 tertiary-text--text">
-                                  <template v-if="lap">{{ lap.runner | fullName }} - <timer :start="Date.parse(lap.start_time)" /></template>
-                                  <template v-else>Nobody</template>
-                                </v-card-text>
-                              </v-col>
+                                <v-progress-circular indeterminate color="primary" />
                             </v-row>
-                          </v-card>
-                        </v-container>
-                      </v-list-item>
-                    </v-list>
-                  </template>
-                </active-runner>
-              </tbody>
-            </v-simple-table>
-          </v-col>
+                            </v-container>
+                            <v-list v-show="!loading" color="tertiary">
+                            <v-list-item>
+                                <v-container>
+                                <v-card class="d-flex" justify="center" color="tertiary">
+                                    <v-row justify="center">
+                                    <v-col>
+                                        <v-card-text class="text-center py-0 tertiary-text--text">
+                                        <template v-if="lap">{{ lap.runner | fullName }} - <timer :start="Date.parse(lap.start_time)" /></template>
+                                        <template v-else>Nobody</template>
+                                        </v-card-text>
+                                    </v-col>
+                                    </v-row>
+                                </v-card>
+                                </v-container>
+                            </v-list-item>
+                            </v-list>
+                        </template>
+                        </active-runner>
+                    </tbody>
+                </v-simple-table>
+            </v-col>
+            <v-col cols="12" sm="10" md="6" lg="3">
+                <v-simple-table class="tertiary">
+                    <thead>
+                        <tr>
+                        <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Top groups</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <top-groups :max="5" dense/>
+                    </tbody>
+                </v-simple-table>
+                <v-simple-table class="tertiary">
+                    <thead>
+                        <tr>
+                            <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Last laps</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <last-lap-list :max="5" dense />
+                    </tbody>
+                </v-simple-table>
+            </v-col>
+            <v-col cols="12" sm="10" md="6" lg="3">
+                <v-simple-table class="tertiary">
+                <thead>
+                    <tr>
+                    <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Most active runners</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <most-active-runners :max="5" dense/>
+                </tbody>
+                </v-simple-table>  
+            </v-col>
+            <v-col cols="12" sm="10" md="6" lg="3">
+            </v-col>
         </v-row>
-        <v-row>
-          <v-col cols="12">
-            <v-simple-table class="tertiary">
-              <thead>
-                <tr>
-                  <th class="text-left text-subtitle-2 tertiary-text--text font-weight-bold">Last laps</th>
-                </tr>
-              </thead>
-              <tbody>
-                <last-lap-list :max="2" dense />
-              </tbody>
-            </v-simple-table>
-          </v-col>
-        </v-row>
-      </v-col>
-      <v-col cols="12" lg="9">
-        <v-sheet class="v-sheet--offset mx-auto" color="primary lighten-1 white--text" elevation="12">
-          <lap-chart :height="350" :classes="['pr-4']"/>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
+    </v-container>
 
 
 </template>
@@ -122,10 +109,10 @@
   import ActiveRunner from "../components/info/ActiveRunner";
   import Timer from "../components/info/LiveTimer";
   import LastLapList from "../components/lists/LastLapList";
-  import LapChart from "../components/charts/LapChart";
+  // import LapChart from "../components/charts/LapChart";
   export default {
     name: "StatisticsView",
-    components: {LapChart, LastLapList, Timer, ActiveRunner, MostActiveRunners, TopGroups, TopRunners, Queue},
+    components: {LastLapList, Timer, ActiveRunner, MostActiveRunners, TopGroups, TopRunners, Queue},
     data: () => ({
       start: 0,
     }),
